769c5c03f4a1ca995a30c42bab005a6ea88dca79
tdawidei <tdawidei@stevens.edu>
2021-03-29 22:01:16 -0400

    Updated GreenThumb.py. Fixed plant variables and added time logging to CSV output

--- GreenThumb.py
+++ GreenThumb.py
@@ -1,50 +1,55 @@
 #from Arduino import Arduino
 import csv
 import os
+from datetime import datetime
+import time
 
-# Below is for testing only
 from random import random
-def getRandomNum(min, max):
+def getRandomNum(min, max): # For testing only. Generates random number between [min, max)
 	return min + (random() * (max - min))
 
 # Main plant class
 class Plant:
-	def __init__(self, name, temp, humidity, soilMoisture, light): # Load main variables and create csv file for plant
+	def __init__(self, name, airTemp, airHumidity, soilTemp, soilMoisture, ambientLight): # Load main variables and create csv file for plant
 		self.__name = name
-		self.__temp = temp
-		self.__humidity = humidity
+		self.__airTemp = airTemp
+		self.__airHumidity = airHumidity
+		self.__soilTemp = soilTemp
 		self.__soilMoisture = soilMoisture
-		self.__light = light
+		self.__ambientLight = ambientLight
 		self.__filename = "./csvFiles/" + self.__name + ".csv"
 		if not os.path.exists('./csvFiles'):
 			os.makedirs('./csvFiles')
-		self.__writeCSVHeaders(self.__filename)
+		self.__writeCSVHeaders()
 
 	def writeCSV(self): # write plants current values to csv
+		time = datetime.now()
 		with open(self.__filename, 'a', newline='') as csvfile:
 			csvwriter = csv.writer(csvfile)
-			csvwriter.writerow([self.__temp, self.__humidity, self.__soilMoisture, self.__light]) 
+			csvwriter.writerow([self.__airTemp, self.__airHumidity, self.__soilTemp, self.__soilMoisture, self.__ambientLight,
+				time.strftime("%d/%m/%Y"), time.hour * 3600 + time.minute + time.second]) 
 
-	def __writeCSVHeaders(self, filename): # write csv headers
+	def __writeCSVHeaders(self): # write csv headers
 		with open(self.__filename, 'w', newline='') as csvfile:
 			csvwriter = csv.writer(csvfile)
-			csvwriter.writerow(["Temperature", "Humidity", "Soil Moisture", "Light"]) 
+			csvwriter.writerow(["Air Temperature", "Air Humidity", "Soil Humidity", "Soil Moisture", "Ambient Light", "Date", "Time"]) 
 
 	# For testing only!
 	def randomizeValues(self): # Randomizes plant values
-		self.__temp = getRandomNum(50,100)
-		self.__humidity = getRandomNum(20,80)
+		self.__airTemp = getRandomNum(50,100)
+		self.__airhumidity = getRandomNum(20,80)
+		self.__soilTemp = getRandomNum(50,100)
 		self.__soilMoisture = getRandomNum(0,100)
-		self.__light = getRandomNum(3000,6000)
+		self.__ambientLight = getRandomNum(3000,6000)
 
 # board = Arduino(115200) # plugged in via USB, serial com at rate 115200
 
 # For testing only!
-plants = [Plant("Dragon Lily", getRandomNum(50,100), getRandomNum(20,80), getRandomNum(0,100), getRandomNum(3000,6000)),
-		Plant("Spider Plant", getRandomNum(50,100), getRandomNum(20,80), getRandomNum(0,100), getRandomNum(3000,6000)),
-		Plant("Jade Plant", getRandomNum(50,100), getRandomNum(20,80), getRandomNum(0,100), getRandomNum(3000,6000)),
-		Plant("Pothos", getRandomNum(50,100), getRandomNum(20,80), getRandomNum(0,100), getRandomNum(3000,6000)),
-		Plant("Asparagus Fern", getRandomNum(50,100), getRandomNum(20,80), getRandomNum(0,100), getRandomNum(3000,6000))]
+plants = [Plant("Dragon Lily", getRandomNum(50,100), getRandomNum(20,80), getRandomNum(50,100), getRandomNum(0,100), getRandomNum(3000,6000)),
+		Plant("Spider Plant", getRandomNum(50,100), getRandomNum(20,80), getRandomNum(50,100), getRandomNum(0,100), getRandomNum(3000,6000)),
+		Plant("Jade Plant", getRandomNum(50,100), getRandomNum(20,80), getRandomNum(50,100), getRandomNum(0,100), getRandomNum(3000,6000)),
+		Plant("Pothos", getRandomNum(50,100), getRandomNum(20,80), getRandomNum(50,100), getRandomNum(0,100), getRandomNum(3000,6000)),
+		Plant("Asparagus Fern", getRandomNum(50,100), getRandomNum(20,80), getRandomNum(50,100), getRandomNum(0,100), getRandomNum(3000,6000))]
 
 for i in range(20):
 	for plant in plants:
@@ -55,5 +60,6 @@ for i in range(20):
 
 # while True:
 # 	for plant in plants:
+#		plant.readSensorData()
 # 		plant.writeCSV()
 # 	time.sleep(300) # Wait for 5 mintues
